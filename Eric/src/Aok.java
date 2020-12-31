b996d9fa1d859b8d887163b18e79fddd9566f86a
Eric Shih <eric12345566@gmail.com>
2020-12-31 19:23:13 +0800

    Add AokRouter

--- Eric/src/Aok.java
+++ Eric/src/Aok.java
@@ -1,5 +1,24 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class Aok {
-    public static void main(String[] args) {
-        System.out.println("");
+    ArrayList<AokRouter> aokRoutersList = new ArrayList<AokRouter>();
+    public void start(int port) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        // TODO: transfer AokRouter to createContext
+        // server.createContext("/post", new PostHandler());
+        // server.createContext("/get", new GetHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    public void addRoute(AokRouter aokRouter){
+        aokRoutersList.add(aokRouter);
     }
 }
