5c04f357a923267dd44792be19a3909b5c0d2135
EriaWist <49594697+EriaWist@users.noreply.github.com>
2021-01-03 15:11:54 +0800

    Update AokRouter.java
    add AokRouter code

--- Eric/src/AokRouter.java
+++ Eric/src/AokRouter.java
@@ -1,3 +1,29 @@
 public abstract class AokRouter {
+  private AokController ctr;
+    AokRouter(AokController ctr){
+        this.ctr = ctr;
+    }
 
+    public void setController(AokController ctr){
+        this.ctr=ctr;
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String htmlResponse = "";
+        if("GET".equals(httpExchange.getRequestMethod())) {
+            htmlResponse=ctr.get();
+        }else if("POST".equals(httpExchange.getRequestMethod())) {//他忘記加上
+            htmlResponse=ctr.post();
+        } else if("PUT".equals(httpExchange.getRequestMethod())) {
+            htmlResponse=ctr.put();
+        }else if("DELETE".equals(httpExchange.getRequestMethod())) {
+            htmlResponse=ctr.delete();
+        }
+        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        OutputStream outputStream = httpExchange.getResponseBody();
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
 }
