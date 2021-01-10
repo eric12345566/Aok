eb2f2d484c1f7866f5ca6e91d072d5da8dad202e
Sisia <D0746490@o265.fcu.edu.tw>
2021-01-10 23:20:51 +0800

    finish Router, Controller

--- Aok/src/Aok/AokController.java
+++ Aok/src/Aok/AokController.java
@@ -1,4 +1,32 @@
 package Aok;
 
+import com.sun.net.httpserver.HttpExchange;
+
 public class AokController {
+  AokView view = null;
+  AokModel model = null;
+  
+  public String Get(HttpExchange exchange) {
+    return "404";
+  }
+  
+  public String Post(HttpExchange exchange) {
+    return "404";
+  }
+  
+  public String Delete(HttpExchange exchange) {
+    return "404";
+  }
+  
+  public String Put(HttpExchange exchange) {
+    return "404";
+  }
+  
+  public void setView(AokView v) {
+    this.view = v;
+  }
+  
+  public void setModel(AokModel m) {
+    this.model = m;
+  }
 }
--- Aok/src/Aok/AokRouter.java
+++ Aok/src/Aok/AokRouter.java
@@ -1,4 +1,47 @@
 package Aok;
 
-public class AokRouter {
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class AokRouter implements HttpHandler {
+  AokController ctr = new AokController();
+  @Override
+  public void handle(HttpExchange exchange) throws IOException {
+    // TODO Auto-generated method stub
+    String htmlResponse = "";
+    if("GET".equals(exchange.getRequestMethod())) {
+      htmlResponse = ctr.Get(exchange);
+    }
+    
+    else if("POST".equals(exchange.getRequestMethod())) { 
+      htmlResponse = ctr.Post(exchange);
+    }
+      
+    else if("DELETE".equals(exchange.getRequestMethod())) { 
+      htmlResponse = ctr.Delete(exchange);
+    }
+      
+    else if("PUT".equals(exchange.getRequestMethod())) { 
+      htmlResponse = ctr.Put(exchange);
+    }
+    
+    exchange.sendResponseHeaders(setStatus(htmlResponse), htmlResponse.length());
+    
+    OutputStream outputStream = exchange.getResponseBody();
+    outputStream.write(htmlResponse.getBytes());
+    outputStream.flush();
+    outputStream.close();
+    
+  }
+  
+  public int setStatus(String s) {
+    if(s.equals("404"))
+      return 404;
+    else
+      return 200;
+  }
+  
 }
