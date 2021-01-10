cd1c1cc6b92c6d7a4eba9806439ac99fdcf0db58
Eric Shih <eric12345566@gmail.com>
2021-01-10 19:05:24 +0800

    try httpserver on Eric

--- Aok/src/Aok/Aok.java
+++ Aok/src/Aok/Aok.java
@@ -1,4 +1,23 @@
 package Aok;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Aok {
+    HttpServer server;
+
+    public void startServer(int port) throws IOException {
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        /*server.createContext("/post", new PostHandler());
+        server.createContext("/get", new GetHandler());*/
+        
+        server.setExecutor(threadPoolExecutor); // creates a default executor
+        server.start();
+    }
 }
