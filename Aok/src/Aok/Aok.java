3a964bf085aac0498da3e4f4a10dc94c5569d014
Eric Shih <eric12345566@gmail.com>
2021-01-10 19:09:51 +0800

    implement Aok

--- Aok/src/Aok/Aok.java
+++ Aok/src/Aok/Aok.java
@@ -1,23 +1,28 @@
 package Aok;
 
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Aok {
     HttpServer server;
+    ArrayList<AokRouter> aokRoutersList = new ArrayList<AokRouter>();
 
     public void startServer(int port) throws IOException {
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
         server = HttpServer.create(new InetSocketAddress(port), 0);
-        /*server.createContext("/post", new PostHandler());
-        server.createContext("/get", new GetHandler());*/
-        
         server.setExecutor(threadPoolExecutor); // creates a default executor
         server.start();
+    }
+
+    public void addRouter(String path, AokRouter handler){
+        server.createContext(path, (HttpHandler) handler);
+        aokRoutersList.add(handler);
     }
 }
